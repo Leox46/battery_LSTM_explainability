12824a92cf969cb5625077b952ba674d49d9a0d0
Leonardo Dal Ronco <leonardo.dalronco@vicker.org>
2024-05-30 21:10:12 +0200

    - Improved README.md

--- 🏠_About.py
+++ 🏠_About.py
@@ -4,10 +4,10 @@ st.markdown(
 """
 # About
 
-This web application is the result of the thesis work carried out by engineer Leonardo Dal Ronco: "Explainable Artificial Intelligence applied to LSTM Neural Networks for lithium-ion batteries capacity estimation".
-The two goals of this thesis work were:
-- Implement an LSTM neural network with the same structure as the network described by [Ansari, S. (2021)](https://www.mdpi.com/2071-1050/13/23/13333), simpler than the ones described by [Choi, Y. (2019)](https://ieeexplore.ieee.org/abstract/document/8731962/) and [Park, K. (2020)](https://ieeexplore.ieee.org/abstract/document/8967059), but able to obtain the same results in terms of accuracy. A simpler network improves the subsequent explainability of the network.
-- Apply explainability techniques to the network obtained previously, in order to provide a tool that can help evaluate the trust that can be placed in this model.
+This web application is the tool presented in the paper entitled "Enhancing Battery Capacity Estimation with an Efficient LSTM Model and Explainability Features".
+The main goals of the presented work are:
+- Propose a novel efficient LSTM model, which is able to perform like the state-of-the-art models, but with a simpler architecture that can enhance its efficiency, training time, and ease the adoption of explainability techniques.
+- Then, apply explainability techniques to the network obtained previously, in order to provide a tool that can help evaluate the trust that can be placed in this model.
 
 The web application is structured as follows:
 - **NASA Battery Dataset**: on this page it is possible to interact with the data contained in the [NASA Li-ion Battery Aging Dataset](https://www.nasa.gov/content/prognostics-center-of-excellence-data-set-repository), used to train the implemented LSTM neural network. This dataset reports the data of 4 batteries, labeled as B0005, B0006, B0007, B0018, that were run through 3 different operational profiles (charge, discharge and impedance) at room temperature.
